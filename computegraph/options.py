5f2ab9cba745764418b5386febeae8f901917290
David Shipman <dshipman@gmail.com>
2022-07-13 16:54:18 +1000

    Better fallback for non-graphviz/plotly systems

--- computegraph/options.py
+++ computegraph/options.py
@@ -1,6 +1,20 @@
+try:
+    import pygraphviz  # noqa: F401
+
+    _use_graphviz = True
+except ImportError:
+    _use_graphviz = False
+
+try:
+    import plotly  # noqa: F401
+
+    _backend = "plotly"
+except ImportError:
+    _backend = "matplotlib"
+
 drawing = {
-    "backend": "plotly",  # May be one of 'plotly', 'matplotlib'
-    "use_graphviz": True,  # Enable or disable use of GraphViz layouts
+    "backend": _backend,  # May be one of 'plotly', 'matplotlib'
+    "use_graphviz": _use_graphviz,  # Enable or disable use of GraphViz layouts
     "plotly": {
         "layout": {
             "width": 800,
